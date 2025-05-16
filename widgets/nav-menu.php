3e1d826696bb8893b43bc375874b716a430da521
Esmond <esmondmccain@gmail.com>
2025-05-15 23:01:55 -0500

    deleted folder, file and moved class

--- widgets/nav-menu.php
+++ widgets/nav-menu.php
@@ -1,28 +1,30 @@
 <?php
 declare(strict_types=1);
-namespace includes\classes\emNavMenuElementor;
+namespace Esmond\ElementorWidgets\Widgets;
 
-if (!class_exists('emNavMenuElementor_class')) {
-    class emNavMenuElementor_class extends \Elementor\Widget_Base
+use Elementor\Widget_Base;
+
+if (!class_exists('Nav_Menu;')) {
+    class Nav_Menu extends Widget_Base
     {
     private $display_menu_id;
 
     public function __construct( $data = [], $args = null ) {
         parent::__construct( $data , $args );
 
-		$theme_version = EMCLIENT_Theme_VERSION;
+		$theme_version = esmond_Theme_VERSION;
 		$nonCache_version = rand( 1, 99999999999 );
 		// Enqueue Main style.
-		wp_enqueue_style( 'emClient-elementor-menu-css', get_stylesheet_directory_uri() . '/assets/css/elementor-menu.css', array(), $nonCache_version );
+		wp_enqueue_style( 'esmond-elementor-menu-css', get_stylesheet_directory_uri() . '/assets/css/elementor-menu.css', array(), $nonCache_version );
 
     }
     
     public function get_name() {
-        return 'emClient-menu';
+        return 'esmond-menu';
     }
 
     public function get_title() {
-        return __( 'EM Client Menu', 'emClient-elementor-widgets' );
+        return __( 'EM Nav Menu', 'esmond-elementor-widgets' );
     }
 
     public function get_icon() {
@@ -30,7 +32,7 @@ if (!class_exists('emNavMenuElementor_class')) {
     }
 
     public function get_categories() {
-        return ['general'];
+        return ['esmond'];
     }
 
     private function get_menus() {
@@ -57,7 +59,7 @@ if (!class_exists('emNavMenuElementor_class')) {
         $this->start_controls_section(
             'menu_selection',
             [
-                'label' => __( 'Menu Selection', 'emClient-elementor-widgets' ),
+                'label' => __( 'Menu Selection', 'esmond-elementor-widgets' ),
                 'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
@@ -65,7 +67,7 @@ if (!class_exists('emNavMenuElementor_class')) {
         $this->add_control(
             'menu',
             [
-                'label'   => __( 'Select Menu', 'emClient-elementor-widgets' ),
+                'label'   => __( 'Select Menu', 'esmond-elementor-widgets' ),
                 'type'    => \Elementor\Controls_Manager::SELECT,
                 'options' => $this->get_menus(),
                 'default' => $this->get_default_slug(),
@@ -79,7 +81,7 @@ if (!class_exists('emNavMenuElementor_class')) {
         $this->start_controls_section(
             'menu_selection_style',
             [
-                'label' => __( 'Menu Style', 'emClient-elementor-widgets' ),
+                'label' => __( 'Menu Style', 'esmond-elementor-widgets' ),
                 'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
             ]
         );
@@ -87,10 +89,10 @@ if (!class_exists('emNavMenuElementor_class')) {
         $this->add_control(
             'menu_bg_color',
             [
-                'label'     => __( 'Background Color', 'emClient-elementor-widgets' ),
+                'label'     => __( 'Background Color', 'esmond-elementor-widgets' ),
                 'type'      => \Elementor\Controls_Manager::COLOR,
                 'selectors' => [
-                    '{{WRAPPER}} .emClient-menu' => 'background-color: {{VALUE}}',
+                    '{{WRAPPER}} .esmond-menu' => 'background-color: {{VALUE}}',
                 ],
             ]
         );
@@ -98,10 +100,10 @@ if (!class_exists('emNavMenuElementor_class')) {
         $this->add_control(
             'menu_text_color',
             [
-                'label'     => __( 'Link Color', 'emClient-elementor-widgets' ),
+                'label'     => __( 'Link Color', 'esmond-elementor-widgets' ),
                 'type'      => \Elementor\Controls_Manager::COLOR,
                 'selectors' => [
-                    '{{WRAPPER}} .emClient-menu a:link' => 'color: {{VALUE}}',
+                    '{{WRAPPER}} .esmond-menu a:link' => 'color: {{VALUE}}',
                 ],
             ]
         );
@@ -109,10 +111,10 @@ if (!class_exists('emNavMenuElementor_class')) {
         $this->add_control(
             'menu_text_color_hover',
             [
-                'label'     => __( 'Link Hover Color', 'emClient-elementor-widgets' ),
+                'label'     => __( 'Link Hover Color', 'esmond-elementor-widgets' ),
                 'type'      => \Elementor\Controls_Manager::COLOR,
                 'selectors' => [
-                    '{{WRAPPER}} .emClient-menu a:hover' => 'color: {{VALUE}}',
+                    '{{WRAPPER}} .esmond-menu a:hover' => 'color: {{VALUE}}',
                 ],
             ]
         );
@@ -120,11 +122,11 @@ if (!class_exists('emNavMenuElementor_class')) {
         $this->add_control(
             'menu_text_font_family',
             [
-                'label'     => __( 'Font Family', 'emClient-elementor-widgets' ),
+                'label'     => __( 'Font Family', 'esmond-elementor-widgets' ),
                 'type'      => \Elementor\Controls_Manager::FONT,
                 'default'   => "'Open Sans', san-serif",
                 'selectors' => [
-                    '{{WRAPPER}} .emClient-menu' => 'font-family: "{{VALUE}}"',
+                    '{{WRAPPER}} .esmond-menu' => 'font-family: "{{VALUE}}"',
                 ],
             ]
         );
@@ -132,7 +134,7 @@ if (!class_exists('emNavMenuElementor_class')) {
         $this->add_control(
             'menu_display_style',
             [
-                'label'   => __( 'Display Style', 'emClient-elementor-widgets' ),
+                'label'   => __( 'Display Style', 'esmond-elementor-widgets' ),
                 'type'    => \Elementor\Controls_Manager::SELECT,
                 'options' => [
                     'block'        => 'Block',
@@ -143,7 +145,7 @@ if (!class_exists('emNavMenuElementor_class')) {
                 ],
                 'default' => 'inline-block',
                 'selectors' => [
-                    '{{WRAPPER}} .emClient-menu a' => 'display: {{VALUE}}',
+                    '{{WRAPPER}} .esmond-menu a' => 'display: {{VALUE}}',
                 ],
             ],
         );
@@ -152,7 +154,7 @@ if (!class_exists('emNavMenuElementor_class')) {
         $this->add_control(
             'link_margin',
             [
-                'label'     => __( 'Link Margin', 'emClient-elementor-widgets' ),
+                'label'     => __( 'Link Margin', 'esmond-elementor-widgets' ),
                 'type'      => \Elementor\Controls_Manager::SLIDER,
                 'size_units' => [ 'px', 'em', '%' ],
                 'range'     => [
@@ -173,7 +175,7 @@ if (!class_exists('emNavMenuElementor_class')) {
                     ],
                 ],
                 'selectors' => [
-                    '{{WRAPPER}} .emClient-menu a' => 'margin: {{SIZE}}{{UNIT}}',
+                    '{{WRAPPER}} .esmond-menu a' => 'margin: {{SIZE}}{{UNIT}}',
                 ],
             ]
         );
@@ -183,7 +185,7 @@ if (!class_exists('emNavMenuElementor_class')) {
     }
 
     public function get_style_depends() {
-        return ['emClient-elementor-menu-css'];
+        return ['esmond-elementor-menu-css'];
     }
 
     // front end.
@@ -196,7 +198,7 @@ if (!class_exists('emNavMenuElementor_class')) {
             array(
                 'menu'       => $this->display_menu_id,
                 'container'  => '',
-                'menu_class' => 'emClient-menu',
+                'menu_class' => 'esmond-menu',
             )
         );
     }
@@ -208,7 +210,7 @@ if (!class_exists('emNavMenuElementor_class')) {
             array(
                 'menu'       => $this->display_menu_id,
                 'container'  => '',
-                'menu_class' => 'emClient-menu',
+                'menu_class' => 'esmond-menu',
             )
         );
     }
@@ -216,4 +218,3 @@ if (!class_exists('emNavMenuElementor_class')) {
 } // Closing bracket for class
 
 }
-    use includes\classes\emNavMenuElementor;
