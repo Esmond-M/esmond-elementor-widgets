5219e679684aadb274dc0ef47cf7e36dcdd984e9
Esmond <esmondmccain@gmail.com>
2025-08-05 16:02:07 -0500

    added new widget for testimonials

---
+++ assets/js/testimonial-carousel.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    $('.testimonial-carousel').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
