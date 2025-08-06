a72dc056d4902385347a0b27162b2420e913b226
Esmond <esmondmccain@gmail.com>
2025-08-06 01:40:20 -0500

    include newsletter sign up widget

--- esmond-elementor-widgets.php
+++ esmond-elementor-widgets.php
@@ -14,6 +14,7 @@ namespace Esmond\ElementorWidgets;
 
 use Esmond\ElementorWidgets\Widgets\FAQ_Accordion_Widget;
 use Esmond\ElementorWidgets\Widgets\Nav_Menu;
+use Esmond\ElementorWidgets\Widgets\Newsletter_Signup_Widget;
 use Esmond\ElementorWidgets\Widgets\Testimonial_Carousel_Widget;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -33,8 +34,22 @@ final class EsmondElementorWidgets {
         add_action( 'plugins_loaded', [ $this, 'init_plugin' ] );
         add_action( 'elementor/elements/categories_registered', [ $this, 'create_new_category' ] );
         add_action( 'elementor/widgets/widgets_registered', [ $this, 'init_widgets' ] );
-    }
 
+        // Handle AJAX for Newsletter Signup
+        add_action('wp_ajax_newsletter_signup',  [ $this, 'esmond_handle_newsletter_signup' ]);
+        add_action('wp_ajax_nopriv_newsletter_signup', [ $this, 'esmond_handle_newsletter_signup'] );
+    }
+    public function esmond_handle_newsletter_signup() {
+
+        $email = isset($_POST['email']) ? sanitize_email($_POST['email']) : '';
+
+        if (!is_email($email)) {
+            wp_send_json_error(['message' => 'Invalid email']);
+        }
+
+
+        wp_send_json_success(['message' => 'Thank you for subscribing!']);
+    }
     public function i18n() {
         load_plugin_textdomain( 'esmond-elementor-widgets' );
     }
@@ -50,16 +65,21 @@ final class EsmondElementorWidgets {
         
     }
 
+
     public function init_widgets() {
 
         // Require the widget class.
         require_once __DIR__ . '/widgets/faq-accordion.php';
         require_once __DIR__ . '/widgets/nav-menu.php';
+        require_once __DIR__ . '/widgets/newsletter-signup.php';        
         require_once __DIR__ . '/widgets/testimonial-carousel.php';
+
         // Register widget with elementor.
         \Elementor\Plugin::instance()->widgets_manager->register( new FAQ_Accordion_Widget() );
         \Elementor\Plugin::instance()->widgets_manager->register( new Nav_Menu() );
+        \Elementor\Plugin::instance()->widgets_manager->register( new Newsletter_Signup_Widget() );        
         \Elementor\Plugin::instance()->widgets_manager->register( new Testimonial_Carousel_Widget() );
+
     }
 
     public static function get_instance() {
